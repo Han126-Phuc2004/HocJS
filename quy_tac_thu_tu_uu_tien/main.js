7870cc3b4da16d5e927b47a64f249cc97c439203
Han126-Phuc2004 <ngungoc09071990@gmail.com>
2025-10-08 23:37:14 +0700

    thu tu uu tien

---
+++ quy_tac_thu_tu_uu_tien/main.js
@@ -0,0 +1,14 @@
+// uu tien ()
+var A = 1;
+B = 2;
+DTB = (A + B) / 2;
+console.log("Diem trung binh la: " + DTB);
+
+// left to right
+var C = 1 + 2 + 3;
+console.log("C = " + C);
+
+// right to left
+var x = y = 3;
+console.log("x = " + x);
+console.log("y = " + y);
