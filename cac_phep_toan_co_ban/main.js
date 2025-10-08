c8ef454559eb3fb6f3e65ca1924321c461052886
Han126-Phuc2004 <ngungoc09071990@gmail.com>
2025-10-08 22:39:34 +0700

    cac phep toan co ban

---
+++ cac_phep_toan_co_ban/main.js
@@ -0,0 +1,15 @@
+//cac phep toan co ban
+var A = 10;
+var B = 20;
+var tong = A + B;
+var hieu = A - B;
+var tich = A * B;
+var thuong = A / B;
+var du = A % B;
+
+//in ket qua
+console.log("Tong: " + tong);
+console.log("Hieu: " + hieu);
+console.log("Tich: " + tich);
+console.log("Thuong: " + thuong);
+console.log("Du: " + du);
