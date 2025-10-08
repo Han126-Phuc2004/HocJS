a8802d6ecc347b06d0647e26cf943e1a35840ed8
Han126-Phuc2004 <ngungoc09071990@gmail.com>
2025-10-08 22:08:02 +0700

    kieu du lieu nguyen thuy

---
+++ cac_kieu_du_lieu_nguyen_thuy/src/script.js
@@ -0,0 +1,27 @@
+let myString = "Hello, World!";
+console.log(myString);
+console.log(typeof myString);
+
+//kieu number
+var soNguyen = 10;
+var soThuc = 10.5;
+console.log(soNguyen);
+console.log(soThuc);
+console.log(typeof soNguyen);
+console.log(typeof soThuc);
+
+
+//kieu boolean
+var check = true; //false
+console.log(check);
+console.log(typeof check);
+
+//kieu undifined khai bao nhung chua gan gia tri
+var x;
+console.log(x);
+console.log(typeof x);
+
+//kieu null khai bao va gan gia tri null
+var y = null;
+console.log(y);
+console.log(typeof y); //kieu object
