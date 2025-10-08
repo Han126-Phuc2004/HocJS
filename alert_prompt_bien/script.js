cc444b7ea08bb8741916ea46bc12ccf1080322d9
Han126-Phuc2004 <ngungoc09071990@gmail.com>
2025-10-08 21:37:16 +0700

    add alert_prompt_bien

---
+++ alert_prompt_bien/script.js
@@ -0,0 +1,26 @@
+//// Empty JavaScript file
+//// alert hien thi hop thoai
+//alert("Hello");
+//
+//// prompt hien thi hop thoai co the nhap du lieu
+//prompt("Enter your name:");
+//
+////khai bao bien
+//var myName;
+//let Age;
+//
+////khoi tao bien
+//var diemToan = "10";
+//let diemLy = 9;
+//const birthYear = 2004;
+//
+////xuat gia tri bien
+//console.log(diemToan);
+//console.log("Diem toan cua ban la: " + diemToan);
+//console.log("Diem Ly cua ban la: " + diemLy);
+//console.log(birthYear);
+
+var myName = prompt("Nhap ten cua ban: ");
+console.log("Ten cua ban la: " + myName);
+console.log("Dang hoc js");
+
