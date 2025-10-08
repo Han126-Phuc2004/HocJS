e9d96e97e290ff0a9f7c29408ca880f7c56d792d
Han126-Phuc2004 <ngungoc09071990@gmail.com>
2025-10-08 22:29:48 +0700

    xuat du lieu voi bien

---
+++ xuat_du_lieu_vs_bien/main.js
@@ -0,0 +1,20 @@
+// xuat du lieu voi bien trong JS
+var soA = 5;
+var soB = 10;
+
+//Cach 1:
+console.log("So A la: " + soA + ", So B la: " + soB);
+
+//Cach 2:
+console.log("So A la:", soA, "So B la:", soB);
+
+//Cach 3:
+console.log(`So A la: ${soA}, so B la: ${soB}`);
+
+//Ex:
+var A = 5;
+var B = 10;
+var kq = A + B;
+console.log("Ket qua phep tinh la: " + kq);
+console.log("Ket qua cua phep tinh la:", kq);
+console.log(`Ket qua cua phep tinh la: ${kq}`);
