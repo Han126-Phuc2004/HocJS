c97736848f7a6b4a8924e1803a0b00f4c0de6909
Han126-Phuc2004 <ngungoc09071990@gmail.com>
2025-10-09 08:52:52 +0700

    toan tu gan += -= *= /=

---
+++ toan_tu_gan/main.js
@@ -0,0 +1,44 @@
+//toan tu gan =
+//let a = 5;
+//let b = 10;
+//let c = a + b; // c = 15
+//console.log(a);
+//console.log(b);
+//console.log("Gia tri cua c:", c);
+
+//gan += -= *= /=
+let  x = 10;
+x += 7;
+console.log(x);
+
+x-= 3;
+console.log(x);
+
+x *= 2;
+console.log(x);
+
+x /= 4;
+console.log(x);
+
+
+//Exercise
+let a = 7;
+a += 3;
+console.log(`a = ${a}`);
+
+let b = 15;
+b -= 6;
+console.log(`b = ${b}`);
+
+let c = 5;
+c *= 4;
+console.log(`c = ${c}`);
+
+let d = 12;
+d /= 2;
+console.log(`d = ${d}`);
+
+let e = 5;
+f = 2;
+e -= (f+1);
+console.log(`e = ${e}`);
