4fa14bf32bd87c77d89bb215fcfa913854996109
Han126-Phuc2004 <ngungoc09071990@gmail.com>
2025-10-09 09:04:05 +0700

    toan tu tang, giam. Tinh frefix truoc, thuc hien phep tinh sau cung tinh postfix.

---
+++ toan_tu_tang_giam/main.js
@@ -0,0 +1,24 @@
+let a = 10;
+let b = 20;
+let c = 30;
+
+a++;
+b--;
+++a;
+++c;
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+let x = 5;
+let y = 15;
+
+let kq = x++ - ++y + 1;
+// ++y = 16 -> 5 - 16 + 1 = -10 --> x++ = 6
+console.log(kq);
+console.log(x);
+console.log(y);
+
+
+
