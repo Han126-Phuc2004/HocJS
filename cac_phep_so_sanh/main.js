3496f8069efc4d9add2a624ebce9f8a254414fdf
Han126-Phuc2004 <ngungoc09071990@gmail.com>
2025-10-09 10:05:45 +0700

    phep so sanh != <> <= >= == ===

---
+++ cac_phep_so_sanh/main.js
@@ -0,0 +1,23 @@
+// Cac phep so sanh: != <> >= <= == ===
+
+let a = 10;
+let b = 10;
+let c = "10";
+
+console.log(typeof b);
+console.log(typeof c);
+
+console.log(a>b);
+console.log(a<b);
+console.log(a<=b);
+console.log(a>=b);
+console.log(a!=b);
+console.log(c!=b); //luu y
+console.log(a==b);
+console.log(a==c);
+console.log(c==a);
+console.log(a===c); //luu y
+console.log("Phan 2");
+console.log(a!==b);
+console.log(a!==c);
+
